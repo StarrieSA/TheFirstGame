866a9cd3da40c729344734549797972bc993b690
StarrieSA <1852517290@qq.com>
2026-03-28 18:38:10 +0800

    更新移动
    left
    right
    down
    up

--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -1,2 +1,12 @@
 extends CharacterBody2D
-var speed = 300
+@export var speed = 5
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		move_local_x(-speed)
+	if Input.is_action_pressed("right"):
+		move_local_x(speed)
+	if Input.is_action_pressed("up"):
+		move_local_y(-speed)
+	if Input.is_action_pressed("down"):
+		move_local_y(speed)
